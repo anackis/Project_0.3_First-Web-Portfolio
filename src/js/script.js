0104c4d531cd4d71fbf80743804100d3b47b62cf
Alex <rengarsanj@gmail.com>
2022-09-15 08:49:08 -0800

    15.09.2022_1

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
+$(document).ready(function() {
+
 // Script for hamburger 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close'),
     overlay = document.querySelector('.menu__overlay'),
+    linkclick = document.querySelectorAll('.menu__link'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
@@ -14,4 +17,43 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('active');
         overlay.classList.toggle('active');
     });
+
+    linkclick.forEach(linkclick => {
+        linkclick.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            overlay.classList.toggle('active');
+        });
+    })
+
+
+});
+
+
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    // if (!$(this).valid()) {
+    // 	return;
+    // }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        // $('#consultation, #order').fadeOut();
+        // $('.overlay, #thanks').fadeIn();
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+
+
+
+
+
 });
